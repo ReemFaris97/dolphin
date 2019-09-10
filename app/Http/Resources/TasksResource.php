607eddac0c861672a99b7fd8b8310347cc0ee2c8
Omar Zain <omarzain012@gmail.com>
2019-09-10 12:12:55 +0200

    Tasks

--- app/Http/Resources/TasksResource.php
+++ app/Http/Resources/TasksResource.php
@@ -26,7 +26,7 @@ class TasksResource extends ResourceCollection
                     'type'=>Task::$types[$q->type],
                     'is_worker_completed'=>is_null($is_worker_completed)?0:1,
                     'actual_type'=>$q->type,
-                    'status'=>$q->taskStatus(),
+                    'status'=>$q->taskStatus($this->id),
               ];
             }),
             'paginate'=>[
