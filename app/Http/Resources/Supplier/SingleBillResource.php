6e66f7a614414a45fa8ddd3d2341d77c56d49a81
Omar Zain <omarzain012@gmail.com>
2019-09-23 11:31:13 +0200

    updating services response prices values

--- app/Http/Resources/Supplier/SingleBillResource.php
+++ app/Http/Resources/Supplier/SingleBillResource.php
@@ -18,20 +18,20 @@ class SingleBillResource extends JsonResource
             'id'=>$this->id,
             'user_name'=>$this->user->name,
             'bill_number'=>$this->name,
-            'date'=>$this->price,
+            'date'=>$this->date,
             'supplier_id'=>$this->supplier_id,
             'payment_method'=>$this->payment_method,
-            'vat'=>$this->vat,
-            'amount_paid'=>$this->amount_paid,
-            'amount_rest'=>$this->amount_rest,
+            'vat'=>(float)$this->vat,
+            'amount_paid'=>(float)$this->amount_paid,
+            'amount_rest'=>(float)$this->amount_rest,
             'offer'=>[
                 'id'=>$this->offer->id,
                 'products'=>$this->offer->offer_products->transform(function ($qu){
                     return [
                         'product_id'=>optional($qu->product)->id,
                         'product_name'=>optional($qu->product)->name,
-                        'quantity'=>$qu->quantity,
-                        'price'=>$qu->price,
+                        'quantity'=>(int)$qu->quantity,
+                        'price'=>(float)$qu->price,
                     ];
                 }),
             ],
