332ffc08113fa630c640b6d6616961c849de89c4
Omar Zain <omarzain012@gmail.com>
2019-10-22 11:59:22 +0200

    updating

--- app/Http/Resources/Supplier/BillsResource.php
+++ app/Http/Resources/Supplier/BillsResource.php
@@ -19,7 +19,7 @@ class BillsResource extends ResourceCollection
                 return [
                     'id'=>$q->id,
                     'user_name'=>$q->user->name,
-                    'bill_number'=>$q->name,
+                    'bill_number'=>optional($q->name) != null ?$q->name :0,
                     'date'=>$q->date,
                     'supplier_id'=>$q->supplier_id,
                     'payment_method'=>$q->payment_method,
