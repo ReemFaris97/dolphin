c4d1ced82acaccb06cd2c23fe612627044a9b002
Omar Zain <omarzain012@gmail.com>
2019-11-13 13:37:57 +0200

    updating

--- app/Models/SupplierPrice.php
+++ app/Models/SupplierPrice.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupplierPrice extends Model
 {
-    protected $fillable =['user_id','product_id','price'];
+    protected $fillable =['user_id','product_id','price','expired_at'];
 
     public function product(){
         return $this->belongsTo(Product::class,'product_id');
