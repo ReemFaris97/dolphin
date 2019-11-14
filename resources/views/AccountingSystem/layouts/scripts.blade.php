6ff924e8a1bcb424a574e5fb37d2eaef78387dc4
sehamsamy <sehamsamy755@gmail.com>
2019-11-14 17:21:41 +0200

    lastAccountingUpdatesTable

--- resources/views/AccountingSystem/layouts/scripts.blade.php
+++ resources/views/AccountingSystem/layouts/scripts.blade.php
@@ -20,4 +20,8 @@
 
 <!-- Bootstrap core CSS -->
 <!-- Material Design Bootstrap -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+{{--<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.css" rel="stylesheet"/>--}}
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
+{{--<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.js"></script>--}}
