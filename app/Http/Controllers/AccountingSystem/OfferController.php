a785040c431960fe0ed9b908efd99d36293ef145
sehamsamy <sehamsamy755@gmail.com>
2019-12-25 10:30:23 +0200

    mail

--- app/Http/Controllers/AccountingSystem/OfferController.php
+++ app/Http/Controllers/AccountingSystem/OfferController.php
@@ -183,14 +183,16 @@ class OfferController extends Controller
         ]);
     }
 
-    public function  notification(Request $request){
+    public function  notification(){
 
-       $inputs=$request->all();
-       dd($inputs);
-       AccountingNotifiaction::create([
-           'client_id'=>$request['client_id'],
-           'package_id'=>$request['package_id'],
-       ]);
+
+        dd("ffffffffffffffff");
+//       $inputs=$request->all();
+//       dd($inputs);
+//       AccountingNotifiaction::create([
+//           'client_id'=>$request['client_id'],
+//           'package_id'=>$request['package_id'],
+//       ]);
 
         return response()->json(['message' => 'تم القبول']);
 
