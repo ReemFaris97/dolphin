d6b2af9cfa754c575608df3e9dcd33ec68164e1b
sehamsamy <sehamsamy755@gmail.com>
2020-01-02 17:55:07 +0200

    mail

--- app/Http/Controllers/AccountingSystem/StoreController.php
+++ app/Http/Controllers/AccountingSystem/StoreController.php
@@ -249,7 +249,7 @@ class StoreController extends Controller
     {
         $store_id = $request['store_id'];
         $stores = AccountingStore::pluck('ar_name', 'id')->toArray();
-        dd($request->all());
+//        dd($request->all());
         $product_store = AccountingProductStore::where('store_id', $store_id)->wheredate('created_at','=',$request['date'])->pluck('product_id')->toArray();
 //        dd($product_store);
         $products = AccountingProduct::whereIn('id',$product_store)->get();
