fdfacddcb43f6cb8f436fcc1afe5f2b1a4e6e30e
sehamsamy <sehamsamy755@gmail.com>
2020-02-19 18:36:11 +0200

    merge

--- resources/views/AccountingSystem/buy_points/products.blade.php
+++ resources/views/AccountingSystem/buy_points/products.blade.php
@@ -1,34 +1,5 @@
 <div class="form-group block-gp">
-<<<<<<< HEAD
-    <label>اسم الصنف </label>
 
-    <select class=" form-control js-example-basic-single"  name="product_id" placeholder="اختر المنتج" id="selectID">
-        @foreach ($products as $product)
-        <?php
-        $producttax=\App\Models\AccountingSystem\AccountingProductTax::where('product_id',$product->id)->first();
-        $units=\App\Models\AccountingSystem\AccountingProductSubUnit::where('product_id',$product->id)->get();
-        $subunits= collect($units);
-        $allunits=json_encode($subunits,JSON_UNESCAPED_UNICODE);
-        // $new=$subunits,ENT_NOQUOTES);
-     ?>
-
-    <option value="{{$product->id}}"
-       data-name="{{$product->name}}"
-       data-price="{{$product->selling_price}}"
-       data-main-unit="{{$product->	main_unit}}"
-       data-bar-code="{{$product->bar_code}}"
-       data-price-has-tax="{{isset($producttax)? $producttax->price_has_tax : 'hasnotaxes' }}"
-       data-total-taxes="{{ isset($producttax)? $product->total_taxes : 'hasnotaxes'}}"
-       data-subunits="{{$allunits}}">
-        {{$product->name}}
-       </option>
-        @endforeach
-
-    </select>
-
- </div>
-
-=======
 <label>اسم الصنف </label>
 
 <select class=" form-control js-example-basic-single"  name="product_id" placeholder="اختر المنتج" id="selectID">
@@ -39,6 +10,17 @@
     $subunits= collect($units);
     $allunits=json_encode($subunits,JSON_UNESCAPED_UNICODE);
     // $new=$subunits,ENT_NOQUOTES);
+
+    $productdiscounts=\App\Models\AccountingSystem\AccountingProductDiscount::where('product_id',$product->id)->get();
+    $discounts= collect($productdiscounts)->transform(function ($q){
+                return [
+                    'discount_type'=>$q->discount_type,
+
+                    'quantity'=>$q->quantity,
+                    'gift_quantity'=>$q->quantity,
+                    'percent'=>$q->percent,
+                ];
+            });
  ?>
 
 <option value="{{$product->id}}"
@@ -48,7 +30,9 @@
    data-bar-code="{{$product->bar_code}}"
    data-price-has-tax="{{isset($producttax)? $producttax->price_has_tax : 'hasnotaxes' }}"
    data-total-taxes="{{ isset($producttax)? $product->total_taxes : 'hasnotaxes'}}"
-   data-subunits="{{$allunits}}">
+   data-subunits="{{$allunits}}"
+data-discount_type="{{$discounts}}"
+   >
     {{$product->name}}
    </option>
     @endforeach
@@ -56,5 +40,4 @@
 </select>
 
 </div>
->>>>>>> fb6f3a0fc59525cb14a655b69a46733f091ab412
 
