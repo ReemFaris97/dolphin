07f406679c7162b29cfcc1bb149f8a082deaab06
sehamsamy <sehamsamy755@gmail.com>
2020-04-13 13:19:25 +0200

    all

--- app/Http/Controllers/AccountingSystem/PurchaseReturnController.php
+++ app/Http/Controllers/AccountingSystem/PurchaseReturnController.php
@@ -176,9 +176,11 @@ class PurchaseReturnController extends Controller
                 ///if-main-unit
                 if($merge['2']!='main-'.$product->id){
                     $productstore=AccountingProductStore::where('store_id',auth()->user()->accounting_store_id)->where('product_id',$merge['0'])->where('unit_id',$merge['2'])->first();
-                    $productstore->update([
-                        'quantity'=>$productstore->quantity - $merge['1'],
-                    ]);
+                    if($productstore) {
+                        $productstore->update([
+                            'quantity' => $productstore->quantity - $merge['1'],
+                        ]);
+                    }
                 }else{
                     $productstore=AccountingProductStore::where('store_id',auth()->user()->accounting_store_id)->where('product_id',$merge['0'])->where('unit_id',Null)->first();
                     if($productstore) {
