9e1f8f8d787563f760f70f0974509f03b8880a2c
sehamsamy <sehamsamy755@gmail.com>
2020-04-14 19:07:41 +0200

    notes

--- app/Http/Controllers/AccountingSystem/PurchaseReturnController.php
+++ app/Http/Controllers/AccountingSystem/PurchaseReturnController.php
@@ -98,7 +98,7 @@ class PurchaseReturnController extends Controller
         $requests['branch_id']=($user->store->model_type=='App\Models\AccountingSystem\AccountingBranch')?$user->store->model_id:Null;
         $requests['company_id']=($user->store->model_type=='App\Models\AccountingSystem\AccountingCompany')?$user->store->model_id:Null;
         if ($requests['total']==Null){
-            $requests['total']=$return->amount;
+            $requests['total']=$return->amount+$requests['totalTaxs'];
         }
         $return->update([
             'bill_num' =>$return->bill_num."-".$return->created_at->toDateString(),
