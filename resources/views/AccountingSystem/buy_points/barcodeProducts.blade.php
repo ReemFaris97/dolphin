d32d9ce387c07c99779df13d68d677516a30f134
eljoe1717 <eljoe1717@gmail.com>
2020-04-19 12:31:34 +0200

    d

--- resources/views/AccountingSystem/buy_points/barcodeProducts.blade.php
+++ resources/views/AccountingSystem/buy_points/barcodeProducts.blade.php
@@ -48,8 +48,7 @@
        data-unit-id="{{$selectd_unit_id}}"
        data-last-price="{{$lastPrice->price_after_tax??0 }}"
        data-average="{{($average)??0 }}"
-       data-product_expiration="{{($product->type=='product_expiration')? '1':'0' }}"
-    >
+       data-product_expiration="{{($product->type=='product_expiration')? '1':'0' }}">
         {{$product->name}} - {{$product->bar_code}}
        </option>
     </select>
