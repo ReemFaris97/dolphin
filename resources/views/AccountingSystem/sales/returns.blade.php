cdcf6d27e3cf053768a1363a845236d8e389be2c
reemfaris <reem.faris97@gmail.com>
2020-06-03 16:04:28 +0200

    scrolllllllll

--- resources/views/AccountingSystem/sales/returns.blade.php
+++ resources/views/AccountingSystem/sales/returns.blade.php
@@ -3,6 +3,13 @@
 @section('parent_title','إدارة نقطه البيع')
 @section('action', URL::route('accounting.categories.index'))
 @section('styles')
+   <!--- start datatable -->
+<link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css
+" rel="stylesheet" type="text/css">
+<!--- end datatable -->
+
     <link href="{{asset('admin/assets/css/jquery.datetimepicker.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('admin/assets/css/all.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('admin/assets/css/bill.css')}}" rel="stylesheet" type="text/css">
@@ -78,7 +85,7 @@
                         <input type="hidden" name="session_id" value="{{$session->id}}">
                         <input type="hidden" name="shift_id" value="{{$session->shift_id}}">
                         <input type="hidden" name="client_id" id="client_id_val">
-                        <table border="1" class="finalTb mabi3at-bill bill-table
+                        <table border="1" class="table datatable-button-init-basic finalTb mabi3at-bill bill-table
 					    {{(getsetting('name_enable_sales')==1) ? 'name_enable':'' }} {{(getsetting('barcode_enable_sales')==1) ? 'barcode_enable':'' }}
                         {{(getsetting('unit_enable_sales')==1) ? 'unit_enable':'' }} {{(getsetting('quantity_enable_sales')==1) ? 'quantity_enable':'' }} {{(getsetting('unit_price_before_enable_sales') == 1) ? 'unit_price_before_enable':''}} {{(getsetting('unit_price_after_enable_sales')==1) ? 'unit_price_after_enable':'' }} {{(getsetting('total_price_before_enable_sales')==1) ? 'total_price_before_enable':'' }} {{(getsetting('total_price_after_enable_sales')==1) ? 'total_price_after_enable':'' }}">
                             <thead>
@@ -284,6 +291,41 @@
     </div>
 @endsection
 @section('scripts')
+  <!--- start datatable -->
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js
+"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js
+"></script>
+
+<script>
+    $(document).ready(function() {
+    var table = $('.table').DataTable( {
+        responsive: true,
+         dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );
+} );
+</script>
+   <!--- scroll to the last table row -->
+<script>
+$('table').on('DOMSubtreeModified', 'tbody', function(){
+    $("tbody").animate({ scrollTop: $('tbody').prop("scrollHeight")}, 1000);
+});
+</script>
+
     <script src="{{asset('admin/assets/js/jquery.datetimepicker.full.min.js')}}"></script>
     <script src="{{asset('admin/assets/js/scanner.js')}}"></script>
     <script>
