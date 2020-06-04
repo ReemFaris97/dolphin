51fda0da931aeb640a06286a416bf898a4fe27b4
sehamsamy <sehamsamy755@gmail.com>
2020-06-04 16:08:13 +0200

    perrm

--- database/migrations/2020_06_04_112923_add_permesions_part2_to_permissions_table.php
+++ database/migrations/2020_06_04_112923_add_permesions_part2_to_permissions_table.php
@@ -16,22 +16,22 @@ class AddPermesionsPart2ToPermissionsTable extends Migration
         Schema::table('permissions', function (Blueprint $table) {
             \Illuminate\Support\Facades\DB::table('permissions')->insert(
                 array(
-                    [ 'id' => '49', 'name' => 'إدارة الشركات', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '50', 'name' => 'إدارة  الفروع', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '51', 'name' => 'إدارة الخزائن', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '52', 'name' => 'إدارة  الاجهزة', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '53', 'name' => 'إدارة الورديات', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '54', 'name' => 'إدارة  المخازن', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '55', 'name' => 'إدارة تصنيفات الاقسام', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '56', 'name' => 'إدارة  الضرائب', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '57', 'name' => 'إدارة المنتجات', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '58', 'name' => 'إدارة  ارفف المنتجات', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '59', 'name' => 'إدارة التقارير', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '60', 'name' => 'تقارير المشتريات ', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '61', 'name' => 'تقارير المبيعات ', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '62', 'name' => 'تقارير الموردين ', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '63', 'name' => 'تقارير المخازن ', 'ar_name' => '-', 'guard_name'=>'web'],
-                    [ 'id' => '64', 'name' => ' الاعدادات العامة', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '88', 'name' => 'إدارة الشركات', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '90', 'name' => 'إدارة  الفروع', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '91', 'name' => 'إدارة الخزائن', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '92', 'name' => 'إدارة  الاجهزة', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '93', 'name' => 'إدارة الورديات', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '94', 'name' => 'إدارة  المخازن', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '95', 'name' => 'إدارة تصنيفات الاقسام', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '96', 'name' => 'إدارة  الضرائب', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '97', 'name' => 'إدارة المنتجات', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '98', 'name' => 'إدارة  ارفف المنتجات', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '99', 'name' => 'إدارة التقارير', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '100', 'name' => 'تقارير المشتريات ', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '101', 'name' => 'تقارير المبيعات ', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '102', 'name' => 'تقارير الموردين ', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '103', 'name' => 'تقارير المخازن ', 'ar_name' => '-', 'guard_name'=>'web'],
+                    [ 'id' => '104', 'name' => ' الاعدادات العامة', 'ar_name' => '-', 'guard_name'=>'web'],
 
 
 
