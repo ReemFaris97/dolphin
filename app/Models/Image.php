26d2d21339e3008cb8efb0a07fd2cdd8c979e290
seham <sehamsamy755@gmail.com>
2020-12-29 15:25:23 +0200

    tasks_api

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $fillable = ['image'];
+    protected $fillable = ['image','user_id'];
 
     public function model()
     {
