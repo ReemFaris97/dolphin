4b32c6fde58a36efcdb6f4f9c1d5633817f611a4
seham <sehamsamy755@gmail.com>
2021-01-13 14:08:24 +0200

    active_rout

--- resources/views/distributor/cars/_form.blade.php
+++ resources/views/distributor/cars/_form.blade.php
@@ -1,14 +1,14 @@
 <div class="m-portlet__body a-smaller-input-wrapper">
 
-    <div class="form-group m-form__group">
-        <label>المندوب</label>
-        <select name="user_id" class="form-control  m-input select2">
-            <option disabled selected>إختار المندوب</option>
-            @foreach($users as $user)
-                <option value="{{$user->id}}" @if(isset($car)) {{$car->user_id == $user->id ? 'selected' :'' }} @endif  >{{$user->name}}</option>
-            @endforeach
-        </select>
-    </div>
+{{--    <div class="form-group m-form__group">--}}
+{{--        <label>المندوب</label>--}}
+{{--        <select name="user_id" class="form-control  m-input select2">--}}
+{{--            <option disabled selected>إختار المندوب</option>--}}
+{{--            @foreach($users as $user)--}}
+{{--                <option value="{{$user->id}}" @if(isset($car)) {{$car->user_id == $user->id ? 'selected' :'' }} @endif  >{{$user->name}}</option>--}}
+{{--            @endforeach--}}
+{{--        </select>--}}
+{{--    </div>--}}
 
     <div class="form-group m-form__group">
         <label>إسم السيارة</label>
