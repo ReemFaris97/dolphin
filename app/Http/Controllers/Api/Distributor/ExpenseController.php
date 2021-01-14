07b42ee1a26ae711b5225cd2fe2857b88620c249
shabandinho <mahmoudshaband@gmail.com>
2021-01-14 12:41:42 +0200

    changes on expenses

--- app/Http/Controllers/Api/Distributor/ExpenseController.php
+++ app/Http/Controllers/Api/Distributor/ExpenseController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Distributor\ExpenseResource;
 use App\Http\Resources\Distributor\ExpensesResource;
 use App\Http\Resources\Distributor\TransactionResource;
+use App\Models\DistributorRoute;
 use App\Models\DistributorTransaction;
 use App\Models\Expense;
 use App\Traits\ApiResponses;
@@ -20,11 +21,11 @@ use Illuminate\Http\Response;
 
 class ExpenseController extends Controller
 {
-    use ApiResponses,ExpenseOperation;
+    use ApiResponses, ExpenseOperation;
 
 
-    public function index(){
-
+    public function index()
+    {
 
 
         if (request('from') != null && \request('to')) {
@@ -32,41 +33,44 @@ class ExpenseController extends Controller
             $from = Carbon::parse(\request('from'));
             $to = Carbon::parse(\request('to'));
 
-            $expenses = Expense::Where('user_id',auth()->user()->id)->whereBetween('date',[$from,$to])
+            $expenses = Expense::Where('user_id', auth()->user()->id)->whereBetween('date', [$from, $to])
                 ->paginate($this->paginateNumber);
-        }
-        else
-        {
-            $expenses = Expense::Where('user_id',auth()->user()->id)
+        } else {
+            $expenses = Expense::Where('user_id', auth()->user()->id)
                 ->paginate($this->paginateNumber);
         }
 
         return $this->apiResponse(new ExpensesResource($expenses));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $rules = [
 
-            'distributor_route_id'=> 'required|integer|exists:distributor_routes,id',
+//            'distributor_route_id'=> 'required|integer|exists:distributor_routes,id',
             'expenditure_clause_id' => 'required|integer|exists:expenditure_clauses,id',
             'expenditure_type_id' => 'required|integer|exists:expenditure_types,id',
-            'date'=>'required|date',
-            'time'=>'required|string',
-            'image' => 'required|mimes:jpg,jpeg,gif,png',
-            'amount'=>'required|numeric',
-            'notes'=>'required|string',
-            'reader_name'=>'nullable|string',
-            'reader_number'=>'nullable|string',
+            'date' => 'required|date',
+            'time' => 'required|string',
+            'image' => 'nullable|mimes:jpg,jpeg,gif,png',
+            'amount' => 'required|numeric',
+            'notes' => 'required|string',
+            'reader_name' => 'nullable|string',
+            'reader_number' => 'nullable|string',
             'reader_image' => 'nullable|mimes:jpg,jpeg,gif,png',
         ];
-        $validation = $this->apiValidation($request,$rules);
+        $validation = $this->apiValidation($request, $rules);
         if ($validation instanceof Response) {
             return $validation;
         }
-            $request['user_id'] = auth()->user()->id;
+        $request['user_id'] = auth()->user()->id;
 
-       $expense = $this->AddExpense($request);
-        return $this->apiResponse(['msg'=>'العملية تمت بنجاح','data'=>new ExpenseResource($expense)]);
+        if (DistributorRoute::where('user_id', auth()->id())->where('is_active', 1)->count() == 0) {
+            return $this->apiResponse(null, ['date' => __("no active route")], 422);
+
+        }
+        $expense = $this->AddExpense($request);
+        return $this->apiResponse(['msg' => 'العملية تمت بنجاح', 'data' => new ExpenseResource($expense)]);
     }
 
 }
