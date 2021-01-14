eab017670a93391834263dad05bb3782651878e4
seham <sehamsamy755@gmail.com>
2021-01-14 14:47:43 +0200

    currentTrips

--- app/Http/Controllers/Api/Distributor/RouteController.php
+++ app/Http/Controllers/Api/Distributor/RouteController.php
@@ -37,7 +37,7 @@ class RouteController extends Controller
 
     public function currentTrips()
     {
-        $routes = DistributorRoute::where('user_id', auth()->user()->id)->where(['is_available' => 1, 'is_active' => 1])->get();
+        $routes = DistributorRoute::where('user_id', auth()->user()->id)->where('is_available' , 1) ->where('is_active',1)->get();
         return $this->apiResponse( MapRoutesResource::collection($routes));
     }
 
