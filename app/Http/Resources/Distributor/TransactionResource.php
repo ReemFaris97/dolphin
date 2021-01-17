1b6eab97100150871fca96012e02fbf2315cda49
shabandinho <mahmoudshaband@gmail.com>
2021-01-17 12:59:02 +0200

    convert to decimal

--- app/Http/Resources/Distributor/TransactionResource.php
+++ app/Http/Resources/Distributor/TransactionResource.php
@@ -9,29 +9,27 @@ class TransactionResource extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'transactions'=>$this->collection->transform(function ($q){
-                /** @var  \App\Models\DistributorTransaction $q */
-
-                ;
+            'transactions' => $this->collection->transform(function ($q) {
+                /** @var  \App\Models\DistributorTransaction $q */;
                 return [
-                    'id'=>$q->id,
-                    'name'=>optional($q->sender)->name,
-                    'amount' => round( $q->amountByType(auth()->user()),2),
+                    'id' => $q->id,
+                    'name' => optional($q->sender)->name,
+                    'amount' => (int)$q->amountByType(auth()->user()),
                     'date' => optional($q->created_at)->format('Y-m-d') ?? '',
                 ];
             }),
-            'paginate'=>[
+            'paginate' => [
                 'total' => $this->total(),
                 'count' => $this->count(),
                 'per_page' => $this->perPage(),
-                'next_page_url'=>$this->nextPageUrl(),
-                'prev_page_url'=>$this->previousPageUrl(),
+                'next_page_url' => $this->nextPageUrl(),
+                'prev_page_url' => $this->previousPageUrl(),
                 'current_page' => $this->currentPage(),
                 'total_pages' => $this->lastPage()
             ]
@@ -42,7 +40,7 @@ class TransactionResource extends ResourceCollection
     public function withResponse($request, $response)
     {
         $originalContent = $response->getOriginalContent();
-        unset($originalContent['links'],$originalContent['meta']);
+        unset($originalContent['links'], $originalContent['meta']);
         $response->setData($originalContent);
     }
 }
