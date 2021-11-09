f737979fbf67f444c24a631b69cb257879e98963
AbdoZaghlool <abdoabdo20192020@gmail.com>
2021-11-09 16:48:44 +0200

    finished products excel import

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -5,44 +5,96 @@ namespace App\Imports;
 use App\Models\AccountingSystem\AccountingProduct;
 use App\Models\AccountingSystem\AccountingProductCategory;
 use App\Models\AccountingSystem\AccountingIndustrial;
+use App\Models\AccountingSystem\AccountingProductBarcode;
+use App\Models\AccountingSystem\AccountingProductMainUnit;
+use App\Models\AccountingSystem\AccountingProductSubUnit;
+use App\Models\AccountingSystem\AccountingProductTax;
+use App\Models\AccountingSystem\AccountingSupplier;
+use DB;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Schema;
 
-use Maatwebsite\Excel\Concerns\ToModel;
-
-class ProductsImport implements ToModel
+class ProductsImport implements ToCollection, WithHeadingRow //, WithChunkReading
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
+    * @param Collection $collection
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-// dd($row);
-        return new AccountingProduct([
-            'name'     => $row[0]??'',
-            'en_name'    => $row[1]??'',
-            'company_id'     => ,
-            'bar_code'    => $row[3]??'',
-            'category_id' => AccountingProductCategory::query()->firstOrCreate(['ar_name'=>$row[4],'en_name'=>$row[4]])->id,
-            // 'industrial_id'=>  AccountingIndustrial::where("name", "like", "%".$row[5]."%"),
-            'color'     => $row[6]??'',
-            'size'    => $row[7]??'',
-            // ''     => $row[8],
-            // ''    => $row[9],
-            // ''     => $row[10],
-            'description' => $row[1]??'',
-            // 'name'     => $row[12],
-            // 'email'    => $row[13],
-            'min_quantity'     => $row[14]??'',
-            'max_quantity'    => $row[15]??'',
-            'main_unit'     => $row[16]??'',
-            'purchasing_price' => $row[17]??'',
-            'selling_price' => $row[18]??'',
-            // ''    => $row[19],
-            // ''    => $row[20],
-            // ''    => $row[21],
-            // ''    => $row[22],
+        Schema::disableForeignKeyConstraints();
+        DB::table('accounting_products')->delete();
 
-        ]);
+        foreach ($rows as $row) {
+            $product_id = DB::table('accounting_products')->insertGetId([
+                'name' => $row['asm_almad'],
+                'en_name' => $row['alasm_allatyny'],
+                'type' => 'product_expiration',
+                'bar_code' => explode(' ', $row['albarkod'])[0],
+                'main_unit' => $row['aloahd'],
+                'selling_price' => $row['mbyaa'],
+                'purchasing_price' => $row['shraaa'],
+                'supplier_id' => optional(AccountingSupplier::where('name', $row['alshrk_almsnaa'])->first())->id,
+                'industrial_id' => AccountingSupplier::firstOrNew(['name'=>$row['alshrk_almsnaa']])->id,
+            ]);
+
+            $this->handleRelatoins($row, $product_id);
+        }
     }
+
+    private function handleRelatoins($row, $product_id)
+    {
+        DB::table('accounting_products_barcodes')->delete();
+
+        $barcodes = explode(' ', $row['albarkod']);
+        foreach ($barcodes as $value) {
+            DB::table('accounting_products_barcodes')->insert(['product_id' => $product_id,'barcode' => $value]);
+        }
+
+        DB::table('accounting_product_taxes')->delete();
+        $taxes = [];
+        $taxes['product_id'] = $product_id;
+        $taxes['price_has_tax'] = $row['aaatmad_alnsb_lldryb'] == 'Y' ? 1:0;
+        $taxes['tax'] = $row['aaatmad_alnsb_lldryb'] == 'Y' ? 1:0;
+        $taxes['tax_value'] = $row['aldryb'];
+        DB::table('accounting_product_taxes')->insert($taxes);
+
+        $units = collect([
+            [
+                'product_id' => $product_id,
+                'name' => $row['aloahd2'],
+                'bar_code' => $row['albarkod2'],
+                'main_unit_present' => $row['altaaadl2']
+            ],
+            [
+                'product_id' => $product_id,
+                'name' => $row['aloahd3'],
+                'bar_code' => $row['albarkod3'],
+                'main_unit_present' => $row['altaaadl3']
+            ],
+            [
+                'product_id' => $product_id,
+                'name' => $row['aloahd4'],
+                'bar_code' => $row['albarkod4'],
+                'main_unit_present' => $row['altaaadl4']
+            ],
+            [
+                'product_id' => $product_id,
+                'name' => $row['aloahd5'],
+                'bar_code' => $row['albarkod5']??null,
+                'main_unit_present' => $row['altaaadl5']
+            ],
+        ])->filter(fn ($i) =>$i['name']!=null);
+
+        DB::table('accounting_products_subUnits')->insert($units->toArray());
+
+        dump("mama ana 5last :D \b ");
+    }
+
+    // public function chunkSize(): int
+    // {
+    //     return 1;
+    // }
 }
