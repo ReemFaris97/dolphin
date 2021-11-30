8d50fb498058aa9494f832943b1dfa7d7a924542
Mohamed Sharaby <moh.sharaby@yahoo.com>
2021-11-30 18:47:18 +0200

    push

---
+++ database/seeders/UpdateProductsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Imports\AccountProductStoreImport;
+use DB;
+use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
+
+class UpdateProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('accounting_product_stores')->truncate();
+        Excel::import(new AccountProductStoreImport() , 'imports/storage.xls');
+    }
+}
