8d42d8b338a00877fcf8676113035a026cc4e75f
shabandinho <mahmoudshaband@gmail.com>
2021-12-02 13:49:10 +0200

    barcode scope accepts null

--- app/Observers/PurchaseReturnObserver.php
+++ app/Observers/PurchaseReturnObserver.php
@@ -12,21 +12,15 @@ use App\Models\AccountingSystem\AccountingSupplierLog;
 
 class PurchaseReturnObserver
 {
-
     public function creating(AccountingPurchaseReturn $purchase)
     {
-
-
-
     }
 
     public function created(AccountingPurchaseReturn $purchase)
     {
-
         $supplier = AccountingSupplier::find($purchase->supplier_id);
         if ($supplier) {
-            if($purchase->payment=='agel') {
-
+            if ($purchase->payment=='agel') {
                 $log=new AccountingSupplierLog();
 
                 $log->supplier_id=$supplier->id;
@@ -34,14 +28,14 @@ class PurchaseReturnObserver
                 $log->amount=$purchase->total;
                 $log->status='debit';
                 $log->type='مرتجعات';
-                $log->new_balance=round($supplier->balance,2);
+                $log->new_balance=round($supplier->balance, 2);
                 $log->save();
 //                dd($log);
             }
         }
 
 
-         $entry=AccountingEntry::create([
+        $entry=AccountingEntry::create([
             'date'=>$purchase->created_at,
             'source'=>' مرتجع مشتريات',
             'type'=>'automatic',
@@ -49,26 +43,24 @@ class PurchaseReturnObserver
             'status'=>'new'
         ]);
 
-        if ($purchase->payment=='agel'){
+        if ($purchase->payment=='agel') {
             //حساب  المشتريات و المورد
-            $toAccount=AccountingAccount::where('supplier_id',$supplier->id)->first();
             AccountingEntryAccount::create([
                 'entry_id'=>$entry->id,
-                'from_account_id'=>$toAccount->id,
+                'from_account_id'=>$supplier->account_id,
                 'to_account_id'=>getsetting('accounting_id_purchases_returns'),
                 'amount'=>$purchase->total,
             ]);
 
             //حساب  المشتريات والمخزون
-            $storeAccount=AccountingAccount::where('store_id',$purchase->store_id)->first();
+            $storeAccount=AccountingAccount::where('store_id', $purchase->store_id)->first();
             AccountingEntryAccount::create([
                 'entry_id'=>$entry->id,
                 'from_account_id'=>getsetting('accounting_id_purchases_returns'),
                 'to_account_id'=>$storeAccount->id,
                 'amount'=>$purchase->total,
             ]);
-
-        }elseif ($purchase->payment=='cash'){
+        } elseif ($purchase->payment=='cash') {
             //حساب  المرتجعات والنقدية
             AccountingEntryAccount::create([
                 'entry_id'=>$entry->id,
@@ -77,7 +69,7 @@ class PurchaseReturnObserver
                 'amount'=>$purchase->total,
             ]);
             //حساب  المرتجعات والمخزون
-            $storeAccount=AccountingAccount::where('store_id',$purchase->store_id)->first();
+            $storeAccount=AccountingAccount::where('store_id', $purchase->store_id)->first();
             AccountingEntryAccount::create([
                 'entry_id'=>$entry->id,
                 'from_account_id'=>getsetting('accounting_id_purchases_returns'),
@@ -85,6 +77,5 @@ class PurchaseReturnObserver
                 'amount'=>$purchase->total,
             ]);
         }
-
     }
 }
