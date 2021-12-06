107fc3691153164923aaf8f99c5b1a765db5fa93
unknown <asmaaesmail0@gmail.com>
2021-12-06 10:49:32 -0800

    edit fatora

--- resources/views/AccountingSystem/sell_points/a4-sale.blade.php
+++ resources/views/AccountingSystem/sell_points/a4-sale.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>طباعة الفاتورة</title>
+
     <link rel="stylesheet" type="text/css" href="{{ asset('dashboard/assets/vendors/base/bill-print.css') }}">
-</head>
-<body>
+
 <div class="m-portlet__body">
     <button type="button" id="print-all">طباعة</button>
     <!--------- start content ---------->
@@ -328,5 +320,3 @@
     })
 </script>
 
-</body>
-</html>
