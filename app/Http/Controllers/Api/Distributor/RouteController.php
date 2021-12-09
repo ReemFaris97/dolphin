bb6855e2d6666c0d8d1dba753c9506731d5459df
shabandinho <mahmoudshaband@gmail.com>
2021-12-09 11:50:17 +0200

    rollback print

--- app/Http/Controllers/Api/Distributor/RouteController.php
+++ app/Http/Controllers/Api/Distributor/RouteController.php
@@ -157,12 +157,10 @@ class RouteController extends Controller
     public function print_bill($id)
     {
         $bill = RouteTripReport::find(decrypt(str_replace('.html', '', $id)));
-        if (request()->html!=null) {
-            $pdf = PDF::setOption('margin-bottom', 0)->setOption('margin-top', 10)
+        $pdf = PDF::setOption('margin-bottom', 0)->setOption('margin-top', 10)
             ->loadView('distributor.bills.api', ['bill'=>$bill])->setPaper('a6');
 
-            return $pdf->download(Str::snake("{$bill->product_total()} $bill->created_at").".pdf");
-        }
+        return $pdf->download(Str::snake("{$bill->product_total()} $bill->created_at").".pdf");
         return view('distributor.bills.api', compact('bill'));
     }
     public function lastBill(Request $request)
