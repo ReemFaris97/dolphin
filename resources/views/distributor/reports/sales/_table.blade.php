cb5ace730be55be0e6b7f7080e13c8aba50a6140
shabandinho <mahmoudshaband@gmail.com>
2021-12-12 13:58:37 +0200

    change quantity to be by package

--- resources/views/distributor/reports/sales/_table.blade.php
+++ resources/views/distributor/reports/sales/_table.blade.php
@@ -1,14 +1,14 @@
 <table class="table table-striped table-bordered edit-th-50 table-responsive ">
     <thead>
     <tr>
-        <!-- <th rowspan="2" class="text-center">  </th> -->
+        <th rowspan="2" class="align-center text-center">الصنف</th>
         @foreach($sales_distributors as $distributor)
-            <th colspan="3">{!!$distributor->name!!}</th>
+            <th colspan="2">{!!$distributor->name!!}</th>
         @endforeach
     </tr>
     <tr>
+        <th class="d-none hidden"></th>
         @foreach($sales_distributors as $name)
-            <th>الصنف</th>
             <th>الكمية</th>
             <th> قيمة العملية</th>
         @endforeach
@@ -19,8 +19,8 @@
         <tr>
             <td>{!!$product_sales_group->first()->product->name!!}</td>
             @foreach($sales_distributors as $dist)
-                <td>{!!$product_sales_group->where('name',$dist->name)->sum('quantity')??0!!}</td>
-                <td>{!!$product_sales_group->where('name',$dist->name)->sum('price')??0!!}</td>
+                <td>{!!$product_sales_group->where('name',$dist->name)->sum('quantity_in_package')??0!!}</td>
+                <td>{!!round($product_sales_group->where('name',$dist->name)->sum('price'),3)??0!!}</td>
             @endforeach
         </tr>
     @endforeach
