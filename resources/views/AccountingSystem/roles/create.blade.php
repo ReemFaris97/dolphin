78ef98198da3fc10cd53cda7b249927fcbc3555e
Asmaa <asmaaesmail0@gmail.com>
2021-12-14 17:20:03 -0800

    print bill

--- resources/views/AccountingSystem/roles/create.blade.php
+++ resources/views/AccountingSystem/roles/create.blade.php
@@ -22,16 +22,78 @@
 
         <div class="panel-body">
 
-                    <form action="{{route('accounting.roles.store')}}" method="Post" class="form phone_validate" >
-                    @csrf
+            <form action="{{route('accounting.roles.store')}}" method="Post" class="form phone_validate" >
+            @csrf
 
-                    @include('AccountingSystem.roles.form')
+            @include('AccountingSystem.roles.form')
 
-                    </form>
+            </form>
+
+            <form action="">
+                <br>
+            <br>
+            <br>
+                <table class="table datatable-button-init-basic validites-table">
+                    <thead>
+                    <tr>
+                        <th>السماحية</th>
+                        <th>الكل</th>
+                        <th>اضافة</th>
+                        <th>تعديل</th>
+                        <th>حذف</th>
+                        <th>بحث</th>
+                        <th>طباعة</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr class="tr-have-checkBox">
+                            <td>الصلاحيات الادارية</td>
+                            <td><input class="selectAll" type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+
+
+                        </tr>
+                        <tr class="tr-have-checkBox">
+                            <td>ادارة النظام</td>
+                            <td><input class="selectAll" type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+                            <td><input type="checkbox" class="switchery" ></td>
+
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </form>
 
         </div>
 
+
+
     </div>
     </div>
 
+@endsection
+@section('scripts')
+<script>
+    $(".selectAll").click(function() {
+      /*   $("input[type=checkbox]").prop("checked", $(this).prop("checked")); */
+      $(this).parents(".tr-have-checkBox").find('input[type=checkbox]').prop("checked", $(this).prop("checked"))
+    });
+
+$("input[type=checkbox]").click(function() {
+	if (!$(this).prop("checked")) {
+        $(this).parents(".tr-have-checkBox").find$(".selectAll").prop("checked", false);
+	}
+});
+</script>
 @endsection
