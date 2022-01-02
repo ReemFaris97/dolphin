a6b85a0f9561f0eb930a095aa450f2c4aa608e09
Mohamed Sharaby <moh.sharaby@yahoo.com>
2022-01-02 17:03:21 +0200

    add empty blade to accounting

--- resources/views/AccountingSystem/empty.blade.php
+++ resources/views/AccountingSystem/empty.blade.php
@@ -1,30 +1,32 @@
-@extends('AccountingSystem.layouts.master')
-@section('title','     ')
-@section('parent_title','       ')
-{{--@section('action', URL::route('accounting.productionLines.index'))--}}
-@section('styles')
-
+@extends('distributor.layouts.app')
+@section('title')
+    Empty
 @endsection
+
+@section('header')
+@endsection
+
+{{--@section('breadcrumb') @php($breadcrumbs=['البنوك'=>route('distributor.banks.index'),])--}}
+{{--@includeWhen(isset($breadcrumbs),'distributor.layouts._breadcrumb', ['breadcrumbs' =>$breadcrumbs ])--}}
+{{--@endsection--}}
 
 @section('content')
-    <div class="panel panel-flat">
-        <div class="panel-heading">
-            <h5 class="panel-title">عرض
-                <div class="btn-group beside-btn-title">    </div>
-            </h5>
-            <div class="heading-elements">
-                <ul class="icons-list">
-                    <li><a data-action="collapse"></a></li>
-                    <li><a data-action="reload"></a></li>
-                    <li><a data-action="close"></a></li>
-                </ul>
+
+    <div class="m-portlet m-portlet--mobile">
+        <div class="m-portlet__head">
+            <div class="m-portlet__head-caption">
+                <div class="m-portlet__head-title">
+                    <h3 class="m-portlet__head-text">
+                         ........
+                    </h3>
+                </div>
             </div>
-        </div>
-        <div class="panel-body">
-            {{-- content--}}
-        </div>
 
+        </div>
+        <div class="m-portlet__body">
+            ... Content ....
+        </div>
     </div>
-
-
 @endsection
+
+
