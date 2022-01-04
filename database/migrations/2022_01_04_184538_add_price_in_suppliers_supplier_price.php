87c33d474ae504f42a544e8ea7afe237f01609ac
abdullahessam <body12324@gmail.com>
2022-01-04 17:46:26 +0200

    add price to pivot table supplier products

---
+++ database/migrations/2022_01_04_184538_add_price_in_suppliers_supplier_price.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPriceInSuppliersSupplierPrice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('suppliers_supplier_price', function (Blueprint $table) {
+            $table->decimal('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('suppliers_supplier_price', function (Blueprint $table) {
+            $table->dropColumn('price');
+        });
+    }
+}
