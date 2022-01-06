33f66db825df17a49c29fa39abe10d703ef9a8fa
Asmaa <asmaaesmail0@gmail.com>
2022-01-05 17:00:44 -0800

    front

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,14 +4,14 @@
     <div class="m-grid__item m-grid__item--fluid m-wrapper">
 
         <!-- BEGIN: Subheader -->
-        <div class="m-subheader ">
+        {{-- <div class="m-subheader ">
             <div class="d-flex align-items-center">
                 <div class="mr-auto">
                     <h3 class="m-subheader__title ">الرئيسية</h3>
                 </div>
 
             </div>
-        </div>
+        </div> --}}
 
         <!-- END: Subheader -->
         <div class="m-content">
