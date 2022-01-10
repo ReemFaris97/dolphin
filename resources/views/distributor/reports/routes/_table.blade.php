2b59d0e3737af395c8f729d391bd026cb2c5dd04
shabandinho <mahmoudshaband@gmail.com>
2022-01-10 17:15:00 +0200

    finish distrbutor route report

--- resources/views/distributor/reports/routes/_table.blade.php
+++ resources/views/distributor/reports/routes/_table.blade.php
@@ -3,14 +3,15 @@
     <tr>
         <th> كود العملية</th>
         <th>  اسم المسار</th>
-        <th>  تاريخ بداية المسار</th>
-        <th> الحالة</th>
         <th>اسم المندوب</th>
+        <th>  تاريخ بداية المسار</th>
+        <th>  تاريخ انتهاء المسار</th>
+        <th> الحالة</th>
         <th>  عدد العملاء</th>
         <th> عدد المزار </th>
         <th> عدد الزيارات المقبولة </th>
         <th> عدد الزيارات المرفوضة </th>
-        <th>  قيمة المصروفات </th>
+        {{-- <th>  قيمة المصروفات </th> --}}
         <th>    الصافى</th>
         <th class="noExport">عمليات</th>
     </tr>
@@ -20,21 +21,21 @@
         <tr>
             <td> {!!$loop->iteration!!} </td>
             <td>{!!$row->name!!}</td>
-            <td>{!!$row->created_at!!}</td>
+            <td>{!!$row->user_name !!}</td>
+            <td>{!!$row->start_at!!}</td>
+            <td>{!!$row->end_at!!}</td>
+            <td>{!!$row->finish_report_id!=null? 'منتهى':'غيرمنتهى' !!}</td>
+            <td>{!!$row->trip_counts!!}</td>
+            <td>{!!$row->trip_invertory_count!!}</td>
+            <td>{!!$row->accept_count!!}</td>
+            <td>{!!$row->refuse_count!!}</td>
 
-            <td>{!!$row->is_finished==0? 'منتهى':'غيرمنتهى' !!}</td>
-            <td>{!!$row->user->name !!}</td>
-            <td>{!!$row->clients()!!}</td>
-            <td>{!!$row->round!!}</td>
-            <td>{!!$row->accepted_trips()!!}</td>
-            <td>{!!$row->refused_trips()!!}</td>
-
-            <td>{!!$row->trips_reports()->sum('expenses')!!}</td>
-            <td>{!!$row->trips_reports()->sum('route_trip_reports.cash')-$row->trips_reports()->sum('expenses') !!}</td>
+            {{-- <td>{!!$row->trips_reports()->sum('expenses')!!}</td> --}}
+            <td>{!!$row->total_money !!}</td>
 
 
 
-            <td><a href="{{route('distributor.reports.routes.show',$row->id) }}" class="btn btn-success">عرض</a></td>
+            <td>{{-- <a href="{{route('distributor.reports.routes.show',$row->dr_id) }}" class="btn btn-success">عرض</a> --}}</td>
 
         </tr>
     @endforeach
