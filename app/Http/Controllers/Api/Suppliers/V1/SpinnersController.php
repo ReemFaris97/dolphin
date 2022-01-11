e5b03fe16e1b849f3607259b621e66f0d29bd502
abdullahessam <body12324@gmail.com>
2022-01-11 14:39:41 +0200

    add spinners

---
+++ app/Http/Controllers/Api/Suppliers/V1/SpinnersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api\Suppliers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SpinnersController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function __invoke(Request $request)
+    {
+        return \responder::success([
+            'statistics'=>[
+                'purchases'=>"100.00",
+                'purchases_returns'=>'100.00',
+                'credit'=>'1000.00'
+            ],
+            'permissions'=>[
+
+            ]
+        ]);
+    }
+}
---
+++ app/Models/Chat/ChatUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Chat;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatUser extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+     return $this->morphTo('user');
+    }
+}
