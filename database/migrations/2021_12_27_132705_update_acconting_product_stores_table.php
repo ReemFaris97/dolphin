65c699e2a7d21dffdd74690e533782dfd4278925
shabandinho <mahmoudshaband@gmail.com>
2022-01-12 11:28:03 +0200

    fix tax

--- database/migrations/2021_12_27_132705_update_acconting_product_stores_table.php
+++ database/migrations/2021_12_27_132705_update_acconting_product_stores_table.php
@@ -14,15 +14,9 @@ class UpdateAccontingProductStoresTable extends Migration
     public function up()
     {
         Schema::table('accounting_product_stores', function (Blueprint $table) {
-<<<<<<< HEAD
-            // $table->nullableMorphs('added_from');
-            // $table->boolean('type')->default(0)->comment('0=>add ,1=>remove')->change()
-            ;
-=======
-//            $table->nullableMorphs('added_from');
-//            $table->boolean('type')->default(0)->comment('0=>add ,1=>remove')->change()
-//            ;
->>>>>>> 06c87c9d7921fde4a7e1777d79c9ba5892bb35e8
+           $table->nullableMorphs('added_from');
+           $table->boolean('type')->default(0)->comment('0=>add ,1=>remove')->change()
+           ;
         });
     }
 
@@ -34,13 +28,8 @@ class UpdateAccontingProductStoresTable extends Migration
     public function down()
     {
         Schema::table('accounting_product_stores', function (Blueprint $table) {
-<<<<<<< HEAD
-            // $table->dropMorphs('added_from');
-            // $table->dropColumn('type');
-=======
-//            $table->dropMorphs('added_from');
-//            $table->dropColumn('type');
->>>>>>> 06c87c9d7921fde4a7e1777d79c9ba5892bb35e8
+           $table->dropMorphs('added_from');
+           $table->dropColumn('type');
         });
     }
 }
