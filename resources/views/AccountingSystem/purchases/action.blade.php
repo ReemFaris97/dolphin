6feb098efa466b7ebe8d18d898796c743fb8f1df
shabandinho <mahmoudshaband@gmail.com>
2022-01-12 13:24:45 +0200

    fix bugs

--- resources/views/AccountingSystem/purchases/action.blade.php
+++ resources/views/AccountingSystem/purchases/action.blade.php
@@ -1,4 +1,5 @@
-<a href="{{route('accounting.purchases.show',$row->id)}}" data-toggle="tooltip" data-original-title="تعديل"> <i class="icon-eye text-inverse" style="margin-left: 10px"></i> </a>
+<a href="{{route('accounting.purchases.show',$row->id)}}" data-toggle="tooltip" data-original-title="مشاهدة"> <i class="icon-eye text-inverse" style="margin-left: 10px"></i> </a>
+<a href="{{route('accounting.purchases.print',$row->id)}}" data-toggle="tooltip" data-original-title="طباعة"> <i class="icon-eye text-inverse" style="margin-left: 10px"></i> </a>
 <a href="{{route('accounting.purchases.edit',$row->id)}}" data-toggle="tooltip" data-original-title="تعديل"> <i class="icon-pencil7 text-inverse" style="margin-left: 10px"></i> </a>
 
 <a href="#" onclick="Delete({{$row->id}})" data-toggle="tooltip" data-original-title="حذف"> <i class="icon-trash text-inverse text-danger" style="margin-left: 10px"></i> </a>
