873f549fb07b5a37edcda408b42b00183622eb91
abdullahessam <body12324@gmail.com>
2022-01-26 16:59:38 +0200

    fix notifications

--- app/Http/Controllers/AccountingSystem/Suppliers/InvoiceController.php
+++ app/Http/Controllers/AccountingSystem/Suppliers/InvoiceController.php
@@ -38,7 +38,7 @@ class InvoiceController extends Controller
             'status'=>'required|in:accept,reject'
         ]);
         $suppliers_invoice->update($inputs);
-        $suppliers_invoice->supplier->admin->notify(new SupplierNotification([
+        \Notification::send($suppliers_invoice->supplier->users,new SupplierNotification([
             'title'=>'تطبيق الموردين',
             'body'=>"تحديث علي حالة عرض السعر رقم $suppliers_invoice->id",
             'type'=>'price_offer',
