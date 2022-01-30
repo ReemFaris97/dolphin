198bd2979c08052fe560759525a6e24a6110c52a
shabandinho <mahmoudshaband@gmail.com>
2022-01-30 14:46:53 +0200

    HOTFIX::exception on product

--- app/Http/Controllers/AccountingSystem/ProductController.php
+++ app/Http/Controllers/AccountingSystem/ProductController.php
@@ -803,20 +803,17 @@ class ProductController extends Controller
             ]);
         }
         $product->tax()->sync([]);
-        if (isset($request['tax']) & $request['tax'] == 1) {
-            if (isset($request['tax_band_id'])) {
-                $taxs = $request['tax_band_id'];
-                foreach ($taxs ??[]as $tax) {
-                    AccountingProductTax::create([
+        $taxs = $request->taxs;
+        foreach ($taxs ??[]as $tax) {
+            AccountingProductTax::create([
                         'product_id' => $product->id,
                         'tax_value' => AccountingTaxBand::find($tax)->percent,
                         'price_has_tax' => 1,
                         'tax_band_id' => $tax,
                         'tax' => $tax,
                     ]);
-                }
-            }
         }
+      
 
         return response()->json(['status' => true, 'message' => 'تم التعديل  بنجاح !']);
     }
